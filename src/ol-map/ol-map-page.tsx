c9104cb417cef1f630a43ba503fe91deaedcdef8
Junhyuck <mrbartrns@naver.com>
2024-07-23 13:52:05 +0900

    feature: add cluster style

--- src/ol-map/ol-map-page.tsx
+++ src/ol-map/ol-map-page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import VectorLayer from './vector-layer';
 import VWorld from './vworld';
 import Marker from './marker';
+import ClusterLayer from './cluster-layer';
+import Cluster from './cluster';
 
 const OlMap = dynamic(() => import('./map'), { ssr: false });
 
@@ -11,13 +12,19 @@ const OlMapPage = () => {
   return (
     <div>
       <div>VWORLD + Open Layers</div>
-      <OlMap height={'600px'} minZoom={6} width={'600px'}>
+      <OlMap height={'600px'} maxZoom={18} minZoom={6} width={'600px'}>
         <VWorld apiKey={process.env.NEXT_PUBLIC_VWORLD_API_KEY} />
-        <VectorLayer minZoom={11} zIndex={10}>
+        {/* <VectorLayer minZoom={11} zIndex={10}>
           <Marker
             position={{ lng: 14135126.30194829, lat: 4518366.509981735 }}
           />
-        </VectorLayer>
+        </VectorLayer> */}
+        <ClusterLayer minZoom={6} zIndex={10}>
+          <Cluster
+            position={{ lng: 14135126.30194829, lat: 4518366.509981735 }}
+          />
+          <Cluster position={{ lng: 14135126.30194, lat: 4518366.509981735 }} />
+        </ClusterLayer>
       </OlMap>
     </div>
   );
